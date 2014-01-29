a7aad9a946b43b95b80aa3531bba11b239a3ee68
macournoyer <macournoyer@gmail.com>
2014-01-28 21:31:17 -0500

    Clear up code.

--- game.js
+++ game.js
@@ -1,26 +1,9 @@
-function Game(canvas) {
-  var self = this
+// The game engine
 
+function Game(canvas) {
   this.context = canvas.getContext("2d")
   this.width = canvas.width
   this.height = canvas.height
-
-  // Key track of pressed keys
-  this.keyDown = {
-    // up: (true while key is down)
-  }
-
-  $(canvas).on('keydown keyup', function(e) {
-    // Convert key code to key name
-    var keyName = Game.keys[e.which]
-
-    if (keyName) {
-      // eg.: `self.keyDown.up = true` if UP key is pressed
-      // Will be set to `false` when the key is released
-      self.keyDown[keyName] = e.type === 'keydown'
-      e.preventDefault()
-    }
-  })
 }
 
 // Some key codes to key name mapping
@@ -30,98 +13,4 @@ Game.keys = {
   38: 'up',
   39: 'right',
   40: 'down'
-}
-
-Game.prototype.update = function() {
-  this.entities.forEach(function(entity) {
-    if (entity.update) entity.update()
-  })
-}
-
-Game.prototype.draw = function() {
-  var self = this
-
-  this.entities.forEach(function(entity) {
-    if (entity.draw) entity.draw(self.context)
-  })
-}
-
-// Start the game loop
-Game.prototype.start = function() {
-  var self = this,
-      fps = 60,
-      interval = 1000 / fps // ms per frame
-
-  setInterval(function() {
-    self.update()
-    self.draw()
-  }, interval)
-}
-
-/////////////////////// PART II ///////////////////////
-
-// A better game loop for languages with timers
-Game.prototype.betterStart = function() {
-  var self = this,
-      fps = 60,
-      interval = 1000 / fps,
-      updated = false
-
-  setInterval(function() {
-    self.update()
-    updated = true
-  }, interval)
-
-  var onFrame = function() {
-    if (updated) self.draw()
-    // Schedule the next update
-    requestAnimationFrame(onFrame)
-  }
-  onFrame()
-}
-
-// A classic game loop you'll find in most games
-Game.prototype.classicStart = function() {
-  var self = this
-
-  this.lastUpdateTime = new Date().getTime()
-  
-  var onFrame = function() {
-    self.fixedTimeStep()
-    // self.variableTimeStep()
-
-    // Schedule the next update
-    requestAnimationFrame(onFrame)
-  }
-  onFrame()
-}
-
-Game.prototype.fixedTimeStep = function() {
-  var currentTime = new Date().getTime(),
-      fps = 60,
-      interval = 1000 / fps,
-      updated = false
-
-  while (this.lastUpdateTime < currentTime) {
-    this.update()
-    updated = true
-    this.lastUpdateTime += interval
-  }
-
-  if (updated) this.draw()
-}
-
-Game.prototype.variableTimeStep = function() {
-  var currentTime = new Date().getTime(),
-      fps = 60,
-      interval = 1000 / fps,
-      timeDelta = currentTime - this.lastUpdateTime,
-      percentageOfInterval = timeDelta / interval
-
-  // FIXME this requires changing the update function to all your entities
-  //       to support partial updating.
-  this.update(percentageOfInterval)
-  this.draw()
-
-  this.lastUpdateTime = new Date().getTime()
 }
