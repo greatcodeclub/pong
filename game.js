4c0bb81cd27cc49d9d84f727a5d3e9d879025680
macournoyer <macournoyer@gmail.com>
2014-01-30 21:57:32 -0500

    Document game loop more thoroughly.

--- game.js
+++ game.js
@@ -47,7 +47,11 @@ Game.prototype.draw = function() {
   })
 }
 
-// A simple (but inaccurate) game loop
+// A simple (but unreliable) game loop.
+//
+// The problem with this approach is that the update and draw operations
+// are glued together. If the timer is not able to run at the proper interval
+// (if CPU is too busy for example), the game will appear sluggish.
 //
 //   Game.prototype.start = function() {
 //     var self = this,
@@ -60,34 +64,44 @@ Game.prototype.draw = function() {
 //     }, interval)
 //   }
 
-// A better game loop. Similar to the ones you'll find in most games.
+// Here is a real game loop. Similar to the ones you'll find in most games.
 Game.prototype.start = function() {
   var self = this
 
   this.lastUpdateTime = new Date().getTime()
   
+  // The loop
   onFrame(function() {
-    // Two modes:
+    // A turn in the loop is called a step.
+    // Two possible modes:
     self.fixedTimeStep()
     // or
     // self.variableTimeStep()
   })
 }
 
-// Calls the `callback` function when we should draw a new frame.
-// Similar to our timer, but this will use the correct FPS to sync with the
-// screen refresh rate.
+// Instead of relying on a timer, we use a special browser function called
+// `requestAnimationFrame(callback)`. It calls the `callback` at interval 
+// synced with the display refresh rate.
 // More info at:
 // https://developer.mozilla.org/en/docs/Web/API/window.requestAnimationFrame
 var onFrame = function(callback) {
-  requestAnimationFrame(function() {
-    callback()
-    // requestAnimationFrame only calls once, we need to schedule the next
-    // call ourself.
-    onFrame(callback)
-  })
+  if (window.requestAnimationFrame) {
+    requestAnimationFrame(function() {
+      callback()
+      // requestAnimationFrame only calls our callback once, we need to
+      // schedule the next call ourself.
+      onFrame(callback)
+    })
+  } else {
+    // requestAnimationFrame is not supported by all browsers. We fall back to
+    // a timer.
+    var fps = 60
+    setInterval(callback, 1000 / fps)
+  }
 }
 
+// With fixed time steps, each update is done at a fixed interval.
 Game.prototype.fixedTimeStep = function() {
   var currentTime = new Date().getTime(),
       fps = 60,
@@ -101,11 +115,14 @@ Game.prototype.fixedTimeStep = function() {
     this.lastUpdateTime += interval
   }
 
-  // No need to draw if nothing was updated, right?
+  // No need to draw if nothing was updated
   if (updated) this.draw()
   updated = false
 }
 
+// With a variable time steps, update are done whenever we need to draw.
+// However we do partial updates. Only updating a percentage of what a fixed
+// time step would normally do.
 Game.prototype.variableTimeStep = function() {
   var currentTime = new Date().getTime(),
       fps = 60,
